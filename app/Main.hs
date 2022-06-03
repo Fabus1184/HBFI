ed47f3fbb3cbc8f837fcad55bf914b06c5596e23
Fabian Lippold <fabianlippold1184@gmail.com>
2022-06-03 13:34:18 +0200

    update

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,32 @@
-module Main where
+module Main (main) where
 
-import Lib
+import Lib (eval)
+
+import Control.Monad (void)
+import Data.List (intercalate)
+import System.Environment (getArgs)
+import System.Exit (exitFailure, exitSuccess)
+import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
-main = someFunc
+main = do
+    k <- getArgs >>= parseArgs
+    c <- readFile k
+    void $ eval ((mempty, 0), c)
+  where
+    parseArgs [] = parseArgs ["--help"]
+    parseArgs ("--help" : xs) = do
+        putStrLn $
+            intercalate
+                "\n"
+                [ "Usage:"
+                , "\thbfi File"
+                , ""
+                , "Options:"
+                , "\t--help\t\t\tshow this message"
+                ]
+        exitSuccess
+    parseArgs [f] = pure f
+    parseArgs xs = do
+        hPutStrLn stderr $ "Unrecognized options: " ++ intercalate "," xs
+        exitFailure
